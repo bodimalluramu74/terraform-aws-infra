9be40ad675b2f985fc3b7843f0c444f8fbd3cb8d
bodimalluramu74 <bodimalluramu74@gmail.com>
2026-06-09 18:12:22 +0530

    new changes

--- main.tf
+++ main.tf
@@ -25,9 +25,12 @@ resource "aws_instance" "test-my-1"{
   }
 }
 
-resource "aws_s3_bucket" "mys3bucket" {
-  bucket = "my-unique-s3-bucket-name-12345"
+resource "aws_s3_bucket" "my_bucket" {
+bucket = "my-unique-s3-bucket-name-12345"
 
-  
+tags = {
+Name = "My bucket"
+Environment = "Dev"
+}
 }
 
